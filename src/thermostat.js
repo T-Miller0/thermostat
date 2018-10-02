61f8c99a224f0bbd555488b1465c8a72d0dfa5b6
tom miller <thomas.j.c.miller@outlook.com>
2018-10-02 15:13:03 +0100

    added minimum temp

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,7 +1,16 @@
 var Thermostat = function(){
   this.temperature = 20;
+  this.minTemp = 10;
 };
 
 Thermostat.prototype.raiseTemp = function(number) {
   return (this.temperature += number)
+};
+
+Thermostat.prototype.decreaseTemp = function(number) {
+  if (this.temperature - number < this.minTemp) {
+    return this.temperature = this.minTemp
+  } else {
+  return (this.temperature -= number)
+  };
 };
